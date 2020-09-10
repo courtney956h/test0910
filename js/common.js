5eb87b3272e356998a8460b119d7cfc609b629e5
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-09-10 16:12:27 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -21,9 +21,9 @@
         $('#kimBox').load(url)
     })
 
-    $('.h1Nav > h1 > a').on('click',function(e) {
+    $('.h1Nav > h1 ').on('click',function(e) {
         e.preventDefault()
-        var url = $(this).attr('href')
+        $('#kimContainer').remove()
         $('#kimBox').load('main.html')
     })
 
